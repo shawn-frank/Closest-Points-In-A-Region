bdb901fa97e79724cc65e98cc232003029562833
shawn-frank <shawn@shawn.design>
2022-01-27 11:04:53 +0400

    update code comments

--- ClosestPairExample/ClosestPairExample/main.swift
+++ ClosestPairExample/ClosestPairExample/main.swift
@@ -48,18 +48,18 @@ struct PointPair: Comparable, Hashable
 
 extension CGPoint
 {
+    // Calculate the distance to another CGPoint
     func distance(to point: CGPoint) -> CGFloat
     {
         return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2))
     }
     
-    
     /// Checks if the current point exists in a region. The x and y coordinate of
     /// `regionStart` has  to be less than or equal to `regionEnd` for a
     /// valid check to occur.
     /// - Parameters:
-    ///   - regionStart: The top left of the region
-    ///   - regionEnd: The bottom right of the region
+    ///   - regionStart: Top left or bottom left of the region based on coordinate system
+    ///   - regionEnd: Bottom right or top left of the region based on coordinate system
     /// - Returns: True if the current point falls within the region
     func doesExistInRegion(regionStart: CGPoint, regionEnd: CGPoint) -> Bool
     {
@@ -83,6 +83,7 @@ extension CGPoint
     }
 }
 
+// Sample points
 let pointsArray = [(0.1, 10.0), (70.0, 10.0), (10.0, 200.0), (70.0, 200.0), (73.0, 10.0), (133.0, 10.0), (73.0, 200.0), (133.0, 200.0), (135.5, 10.0), (195.5, 10.0), (135.5, 200.0), (195.5, 200.0), (198.5, 10.0), (258.5, 10.0), (198.5, 200.0), (258.5, 200.0), (261.5, 10.0), (321.5, 10.0), (261.5, 200.0), (321.5, 200.0), (324.0, 10.0), (384.0, 10.0), (324.0, 200.0), (384.0, 200.0), (387.0, 10.0), (447.0, 10.0), (387.0, 200.0), (447.0, 200.0), (450.0, 10.0), (510.0, 10.0), (450.0, 200.0), (510.0, 200.0), (512.5, 10.0), (572.5, 10.0), (512.5, 200.0), (572.5, 200.0), (575.5, 10.0), (635.5, 10.0), (575.5, 200.0), (635.5, 200.0), (638.5, 10.0), (698.5, 10.0), (638.5, 200.0), (698.5, 200.0), (701.0, 10.0), (761.0, 10.0), (701.0, 200.0), (761.0, 200.0), (764.0, 10.0), (824.0, 10.0), (764.0, 200.0), (824.0, 200.0), (10.0, 390.0), (70.0, 390.0), (73.0, 390.0), (133.0, 390.0), (135.5, 390.0), (195.5, 390.0), (198.5, 390.0), (258.5, 390.0), (261.5, 390.0), (321.5, 390.0), (324.0, 390.0), (384.0, 390.0), (387.0, 390.0), (447.0, 390.0), (450.0, 390.0), (510.0, 390.0), (512.5, 390.0), (572.5, 390.0), (575.5, 390.0), (635.5, 390.0), (638.5, 390.0), (698.5, 390.0), (701.0, 390.0), (761.0, 390.0), (764.0, 390.0), (824.0, 390.0), (10.0, 580.0), (70.0, 580.0), (73.0, 580.0), (133.0, 580.0), (135.5, 580.0), (195.5, 580.0), (198.5, 580.0), (258.5, 580.0)]
 
 let startPoint = CGPoint(x: 80, y: 20)
